6f7d6490e305336aeaf6ff6b22a7fc33ff8df84b
flaging <liyupeng341@163.com>
2024-11-13 00:34:31 +0800

    [feat]: add arxiv link extractor to repo

---
+++ arxiv-link-extrator.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         arxiv Link Extractor
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  在打开axriv.org的某篇论文,自动在新标签页打开对应论文的paper.cool(kimi总结)和alphaxiv.org(论文讨论)内容 
+// @author       flaging
+// @match        https://arxiv.org/abs/*
+// @match        https://arxiv.org/pdf/*
+// @grant        none
+// @license MIT
+// ==/UserScript==
+(function() {
+    'use strict';
+
+    // 获取目标元素
+    const targetElement = document.getElementById('download-button-info');
+    if (targetElement) {
+        // 创建超链接元素
+
+        const link = document.createElement('a');
+        var url = window.location.href.substr(22);
+        window.open('https://papers.cool/arxiv/'+url, '_blank');
+        window.open('https://alphaxiv.org/pdf/'+url, '_blank');
+    }
+})();
